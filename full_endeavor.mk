5229794ad67df75f4963b5d0e91466952fff061e
intermediaterepresentation <intermediaterepresentation@gmail.com>
2012-05-20 23:19:17 -0600

    add bluetooth support

--- full_endeavor.mk
+++ full_endeavor.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+		device/htc/endeavor/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 		frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 		frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 		frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
