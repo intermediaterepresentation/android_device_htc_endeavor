801fa397ea7c7de02c3806441e2d58dde908861f
Tom Gascoigne <tom@gascoigne.me>
2012-05-30 21:17:11 +0100

    Explicitly specified lun file path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,9 @@ TARGET_PROVIDES_INIT_TARGET_RC := true
 
 # vold
 BOARD_VOLD_MAX_PARTITIONS := 20
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/fsl-tegra-udc/gadget/lun0/file
 
 # partitions
 TARGET_USERIMAGES_USE_EXT4 := true
