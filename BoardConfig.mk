f493231cc48b7b60f5357c94beaad55b1f8a8851
Tom Gascoigne <tom@gascoigne.me>
2012-05-30 22:33:57 +0100

    Re-added BOARD_VOLD_EMMC_SHARES_DEV_MAJOR.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_PROVIDES_INIT_TARGET_RC := true
 
 # vold
 BOARD_VOLD_MAX_PARTITIONS := 20
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/fsl-tegra-udc/gadget/lun0/file
