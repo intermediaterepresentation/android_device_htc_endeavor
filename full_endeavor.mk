55e8507201eabd463a58476b9e5fff782d875872
Nagaprasad N <gorbi16@gmail.com>
2012-06-12 22:33:41 +0530

    Minor cleanup.

--- full_endeavor.mk
+++ full_endeavor.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
 		
 # Permissions
 PRODUCT_COPY_FILES += \
-	device/htc/endeavoru/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
