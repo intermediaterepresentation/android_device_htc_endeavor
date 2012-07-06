2f3410c3522743cd57e297bb72cc8f7b431faf53
Tom Gascoigne <tom@gascoigne.me>
2012-07-06 01:57:47 +0100

    Seperate full_endeavoru and device.mk

--- full_endeavoru.mk
+++ full_endeavoru.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2012 The Android Open-Source Project
+# Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,153 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
-# NEED TO ADD acp_recvy and miniunz for some reason
-
-# # This device is xhdpi.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-PRODUCT_LOCALES += en_US xhdpi
-
-# Init files
-PRODUCT_COPY_FILES := \
-	$(LOCAL_PATH)/ramdisk/init:root/init \
-	$(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
-	$(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
-	$(LOCAL_PATH)/ramdisk/init.endeavoru.rc:root/init.endeavoru.rc \
-	$(LOCAL_PATH)/ramdisk/init.endeavoru.common.rc:root/init.endeavoru.common.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.endeavoru.rc:root/ueventd.endeavoru.rc
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-	$(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-	$(LOCAL_PATH)/configs/hostapd.conf:system/etc/wifi/hostapd.conf \
-	$(LOCAL_PATH)/configs/nvcamera.conf:system/etc/nvcamera.conf \
-	$(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
-	$(LOCAL_PATH)/configs/AIC3008_REG_DualMic_XC.csv:system/etc/AIC3008_REG_DualMic_XC.csv \
-	$(LOCAL_PATH)/configs/AIC3008_REG_DualMic.csv:system/etc/AIC3008_REG_DualMic.csv \
-	$(LOCAL_PATH)/configs/alsa.conf:system/usr/share/alsa/alsa.conf
-
-# GPS Certificate
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/etc/SuplRootCert:system/etc/SuplRootCert
-
-# Wifi
-PRODUCT_PACKAGES += \
-	dhcpcd.conf \
-	TQS_D_1.7.ini \
-	calibrator
-
-# Wifi Modules
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/modules/cfg80211.ko:system/lib/modules/cfg80211.ko \
-	$(LOCAL_PATH)/prebuilt/modules/compat.ko:system/lib/modules/compat.ko \
-	$(LOCAL_PATH)/prebuilt/modules/cavm_sqos_mod.ko:system/lib/modules/cavm_sqos_mod.ko \
-	$(LOCAL_PATH)/prebuilt/modules/htc_sqos_ctrlmsg.ko:system/lib/modules/htc_sqos_ctrlmsg.ko \
-	$(LOCAL_PATH)/prebuilt/modules/htc_sqos_encoder.ko:system/lib/modules/htc_sqos_encoder.ko \
-	$(LOCAL_PATH)/prebuilt/modules/htc_sqos_wifi.ko:system/lib/modules/htc_sqos_wifi.ko \
-	$(LOCAL_PATH)/prebuilt/modules/mac80211.ko:system/lib/modules/mac80211.ko \
-	$(LOCAL_PATH)/prebuilt/modules/wl12xx.ko:system/lib/modules/wl12xx.ko \
-	$(LOCAL_PATH)/prebuilt/modules/wl12xx_sdio.ko:system/lib/modules/wl12xx_sdio.ko
-
-# Vold
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab
-
-# Lights
-PRODUCT_PACKAGES += \
-	lights.tegra
-
-# Keyboard
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
-
-# NFC
-PRODUCT_PACKAGES += \
-	libnfc \
-	libnfc_jni \
-	Nfc \
-	Tag \
-	com.android.nfc_extras \
-	com.android.future.usb.accessory
-
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-	LiveWallpapers \
-	LiveWallpapersPicker \
-	VisualizationWallpapers \
-	librs_jni
-
-# a2dp
-PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	libaudioutils \
-	libtinyalsa	
-
-# NFC firmware
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/lib/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
-
-# TripNRaver's libmedia + libaudioflinger
-# Part of the echo fix (stops camera from breaking)
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/lib/libmedia.so:system/lib/libmedia.so \
-	$(LOCAL_PATH)/prebuilt/lib/libaudioflinger.so:system/lib/libaudioflinger.so
-
-# Modified libcamera_client (ignores invalid face recognition parcels)
-# see https://github.com/tgascoigne/android_frameworks_base/commit/3f0067e
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/lib/libcamera_client.so:system/lib/libcamera_client.so
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-	make_ext4fs \
-	setup_fs
-
-# Torch
-PRODUCT_PACKAGES += \
-	Torch		
-		
-# Permissions
-PRODUCT_COPY_FILES += \
-	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
-
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/usr/idc/atmel-maxtouch.idc:system/usr/idc/atmel-maxtouch.idc \
-	$(LOCAL_PATH)/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
-	$(LOCAL_PATH)/prebuilt/usr/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc
-
-# Custom media config for HTC camera
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=320 \
-	wifi.interface=wlan0 \
-	wifi.softap.interface=wlan1 \
-	wifi.softapconcurrent.interface=wlan1 \
-	persist.sys.usb.config=mass_storage,adb
-
-$(call inherit-product-if-exists, hardware/ti/wan/mac80211/Android.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, vendor/htc/endeavoru/endeavoru-vendor.mk)
-$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+$(call inherit-product, device/htc/endeavoru/device.mk)
+
+PRODUCT_NAME := full_endeavoru
+PRODUCT_DEVICE := endeavoru
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Full Android on Endeavoru
